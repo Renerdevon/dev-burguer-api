50e20629f93278fd0d560315b94385106f6fab30
renerdevon <renertic@gmail.com>
2025-12-05 21:05:50 +0000

    back end

--- src/routes.js
+++ src/routes.js
@@ -10,41 +10,50 @@ import adminMiddleware from './app/middlewares/admin.js';
 import OrderController from './app/controllers/OrderController.js';
 
 const routes = new Router();
-
 const upload = multer(multerConfig);
 
+// ROTAS PÚBLICAS
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+// 🔓 Estas duas precisam ser públicas para o cardápio carregar
+routes.get('/products', ProductController.index);
+routes.get('/categories', CategoryController.index);
+
+// Daqui pra baixo → precisa estar autenticado
 routes.use(authMiddleware);
+
+// PRODUTOS
 routes.post(
   '/products',
   adminMiddleware,
   upload.single('file'),
   ProductController.store,
 );
+
 routes.put(
   '/products/:id',
   adminMiddleware,
   upload.single('file'),
   ProductController.update,
 );
-routes.get('/products', ProductController.index);
 
+// CATEGORIAS
 routes.post(
   '/categories',
   adminMiddleware,
   upload.single('file'),
   CategoryController.store,
 );
+
 routes.put(
   '/categories/:id',
   adminMiddleware,
   upload.single('file'),
   CategoryController.update,
 );
-routes.get('/categories', CategoryController.index);
 
+// PEDIDOS
 routes.post('/orders', OrderController.store);
 routes.get('/orders', OrderController.index);
 routes.put('/orders/:id', adminMiddleware, OrderController.update);
