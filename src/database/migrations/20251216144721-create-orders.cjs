fa71968312a7bea16376ad538f7a1035731fcfd7
renerdevon <renertic@gmail.com>
2025-12-19 17:49:01 +0000

    Projeto finalizado.

---
+++ src/database/migrations/20251216144721-create-orders.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('orders');
+  }
+};
